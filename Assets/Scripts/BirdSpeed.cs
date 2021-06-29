797c9c457e8bf8e7bdbb8b5d907e33418a8121d0
Naishvi <44271604+Naishvi@users.noreply.github.com>
2021-06-29 11:39:09 -0400

    Updates
    Snow falling
    Bird with sounds
    Other small changes

---
+++ Assets/Scripts/BirdSpeed.cs
@@ -0,0 +1,36 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Enemy Class.
+/// Contains movement and collision detection for the spawned enemy gameobjects.
+/// </summary>
+[RequireComponent(typeof(Collider))]
+public class BirdSpeed : MonoBehaviour
+{
+
+    private float speed;
+
+    void Start()
+    {
+        //pick random value between min and max speed
+        speed = 5.0f;
+
+    }
+
+    void Update()
+    {
+        //translate this object down at speed
+        this.transform.Translate(Vector3.left * speed * Time.deltaTime);
+    }
+
+    ///
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+
+
+     
+    }
+}
--- Assets/Scripts/Flying.cs
+++ Assets/Scripts/Flying.cs
@@ -1,24 +1,73 @@
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Flying : MonoBehaviour
+public class Flying: MonoBehaviour
 {
-    public float speed = 5.0f; 
-    private Rigidbody2D rb; 
-    private Vector2 screenBounds; 
+
+    /// <summary>
+    /// Array that contains all of the currently active enemy objects.
+    /// </summary>
+    public GameObject[] bird;
+
+    //private float min = -5f, max = 5f;
+
+    /// <summary>
+    /// Time to wait between spawning enemy objects.
+    /// </summary>
+    public float timeBetweenBirds;
+
+    /// <summary>
+    /// Minium allowable time between enemy spawn.
+    /// </summary>
+    public float minBirdTime;
+
+    /// <summary>
+    /// Amount of time to decrease the time between spawning over time.
+    /// </summary>
+
+    /// <summary>
+    /// Triggers spawn when the value is <=0
+    /// </summary>
+    private float BirdTimer;
+
+
+    /// <summary>
+    /// sets the spawning timer to 0 so that an enemy will spawn immediately.
+    /// </summary>
     void Start()
     {
-        rb = this.GetComponent<Rigidbody2D>(); 
-        rb.velocity = new Vector2 (-speed, 0); 
-        screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));
+        BirdTimer = 0f;
     }
 
+    /// <summary>
+    /// Spawns the enemy gameobjects. Uses the spawnTimer to determin when to
+    /// spawn. If the player is dead, no spawns take place. As the game progresses,
+    /// the time between spawns will decrease until a minimum value is reached.
+    /// </summary>
     void Update()
     {
-        if(transform.position.x < screenBounds.x * 2)
-        {
-            Destroy(this.gameObject);
+        
+        //stop spawning if there is no character
+        if (GameObject.FindGameObjectWithTag("Player") == null || GameObject.FindGameObjectWithTag("Player").activeSelf==false) {
+            return;
         }
+        if (BirdTimer <= 0) {
+            float min = -5f;
+            float max = 5f; 
+            //spawn an enemy
+            GameObject birdy = bird[Random.Range(0, bird.Length)];
+            float value = Random.Range(min, max);
+            Vector3 position = new Vector3(value, 6.5f, 0f);
+            Instantiate(birdy,new Vector3(position.x,position.y,0f),Quaternion.identity);
+
+            //increasing difficulty after every spawn
+           
+
     }
+
+   
+  
+}
 }
